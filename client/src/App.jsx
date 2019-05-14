fb4404c7520c3e213fd139d3641b987753cab3d4
Matty-Horton <mthorton1@sheffield.ac.uk>
2019-05-14 12:08:56 +0100

    Displaying all stories on feed when user is logged in

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prop-types */
 import React from 'react';
-
+import axios from 'axios';
 import Layout from './components/Layout';
 import { STORIES_STORE_NAME } from './db/db';
 import './index.css';
@@ -10,15 +10,89 @@ import {
 } from './styles/buttonStyles';
 import Banner from './components/Banner';
 import Features from './components/Features';
+import Story from './components/Stories/Story';
 
-const App = (props) => {
-  const {home} = props;
-  return (
-    <Layout title="Festival" home={home}>
-      <Banner />
-      <Features />
-    </Layout>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      loggedIn: false,
+      stories: [],
+    }
+  }
+
+  async componentDidMount() {
+    const token = localStorage.getItem('JWT');
+    if (token == null) {
+      this.setState({
+        loggedIn: false,
+      });
+      return;
+    }
+    await axios.get('/api/users/find', {
+      headers: {
+        Authorization: `JWT ${token}`,
+      },
+    }).then((res) => {
+      const { data } = res;
+      const {
+        username,
+      } = data;
+      this.setState({
+        username,
+        loggedIn: true,
+      });
+    }).catch((err) => {
+      console.error(err.response.data);
+      this.setState({
+        loggedIn: false,
+      });
+    });
+
+    await this.getStories();
+  }
+
+  getStories = async () => {
+    const token = localStorage.getItem('JWT');
+    await axios.get(`/api/stories/all`, {
+      headers: {
+        Authorization: `JWT ${token}`,
+      },
+    })
+      .then((res) => {
+        this.setState({
+          stories: res.data.stories,
+        });
+      })
+      .catch(() => {
+        this.setState({
+          error: true,
+        });
+      });
+  }
+
+  render() {
+    const { home } = this.props;
+    const { stories } = this.state;
+
+    if (this.state.loggedIn) {
+      return (
+        <Layout title="Festival" home={home}>
+          <div className="event-center-bounds">
+            {stories && stories.map(story => <Story {...story} />)}
+          </div>
+        </Layout>
+      )
+    } else {
+      return (
+        <Layout title="Festival" home={home}>
+          <Banner />
+          <Features />
+        </Layout>
+      );
+    }
+  }
 };
 
 export default App;
