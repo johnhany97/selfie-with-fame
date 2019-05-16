079c4e5bc59acd229ba92b5ff51588cb8743f3f4
Matty-Horton <mthorton1@sheffield.ac.uk>
2019-05-16 16:24:56 +0100

    Added events to discover page

--- client/src/pages/DiscoverPage/index.jsx
+++ client/src/pages/DiscoverPage/index.jsx
@@ -1,18 +1,79 @@
 import React from 'react';
-
+import axios from 'axios';
 import './index.css';
 import Layout from '../../components/Layout';
+import DiscoverEvent from '../../components/DiscoverEvent';
+import Event from '../../components/Event';
 
-class DiscoverPage extends React.Component{
-    constructor(props){
+class DiscoverPage extends React.Component {
+    constructor(props) {
         super(props);
 
+        this.state = {
+            events: [],
+        }
     }
 
-    render(){
-        return(
+    async componentDidMount() {
+        await this.getEvents();
+    }
+
+    getEvents = async () => {
+        const token = localStorage.getItem('JWT');
+        if (token == null) {
+            this.setState({
+                error: true,
+                // isLoading: false,
+            });
+            return;
+        }
+        axios.get('/api/events/getEvents', {
+            params: {
+            },
+            headers: {
+                Authorization: `JWT ${token}`,
+            },
+        }).then((res) => {
+            const { data } = res;
+            const {
+                events,
+            } = data;
+            this.setState({
+                events,
+                // isLoading: false,
+                error: false,
+                // event_deleted,
+            });
+        }).catch((err) => {
+            console.error(err.response.data);
+            this.setState({
+                error: true,
+            });
+        });
+    }
+
+
+    render() {
+        const { events } = this.state;
+        return (
             <Layout title="Event page">
-                <h1>This is the discover page</h1>
+                <div className="container-lg discover-container">
+                    <h3 className="discover-heading">Discover the lastest events</h3>
+                    <div className="container-discover-events scrolling-wrapper">
+                        {events && events.map(event => <DiscoverEvent key={event._id} {...event} />)}
+                    </div>
+                    <h3 className="discover-heading">Discover an event near you</h3>
+                    <div className="event-results">
+                        {events.map(event => (
+                            <div className="event-container">
+                                <Event
+                                    key={event._id}
+                                    {...event}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </Layout>
         );
     }
