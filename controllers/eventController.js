0c29390897d0bdc74b2829e2e03d9f64fb6720d4
Schobs <laschobs1@sheffield.ac.uk>
2019-05-17 23:06:59 +0100

    commented events controller

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -3,6 +3,27 @@ var Event = require('../models/event');
 const validator = require('validator');
 
 
+/**
+ * GET '/api/events/getEvents'
+
+ *
+ * @summary
+ * Gets all events and sorts them in reverse order of creation
+ * 
+ * @auth
+ * Does require authentication
+ *
+ * @param
+ * - none
+ *
+ * @returns:
+ * - Status 200 if successfully got events
+ * If successful, it returns a JSON of the following format:
+ * {
+ *  [Event]
+ * }
+*/
+
 
 module.exports.getEvents = (req, res, next) => {
   Event.find({})
@@ -14,6 +35,31 @@ module.exports.getEvents = (req, res, next) => {
     });
 }
 
+/**
+ * POST '/api/events/createEvent
+ *
+ * @summary
+ * Register on the platform
+ *
+ * @auth
+ * Does require authentication 
+ *
+ * @param
+ * - name => string   - name of event
+ * - information  => string   - information
+ * - start_date      => string   - start date of event
+ * - end_date   => string   - end date of event
+ * - location   => {string, /[float /]}   - a string with the city and an array of coordinates
+ *
+ * @returns:
+ * - Status 20 if event created successfully
+ * - Status 400 if bad request
+ * If successful, it returns a JSON of the following format:
+ * {
+ *  message: "event created", event_id: event_id
+ * }
+*/
+
 module.exports.createEvent = (req, res, next) => {
   const event = new Event({
     name: req.body.name,
@@ -23,7 +69,6 @@ module.exports.createEvent = (req, res, next) => {
     location: { "coordinates": req.body.location, "city": req.body.city }
   });
 
-  //    location: {"coordinates": [req.body.location[0] + -.00004 * Math.cos((+a*i) / 180 * Math.PI),req.body.location[1]+ -.00004 * Math.cos((+a*i) / 180 * Math.PI)] "city": req.body.city}
 
 
   event.save()
@@ -37,10 +82,25 @@ module.exports.createEvent = (req, res, next) => {
     });
 }
 
-module.exports.getStory = (req, res, next) => {
-  res.status(500).send('Not implemented yet');
-}
 
+/**
+ * DELETE /api/events/:_id
+ * 
+ *
+ * @summary
+ * Deletes an event given its id
+ *
+ * @auth
+ * Requires signed in user that created event or admin
+ *
+ * @param
+ * - id => String   - event id
+ *
+ * @returns:
+ * - Status 200 if successfully deleted
+ * - Status 404 if event not found
+ * - Status 500 if error with deleting event
+*/
 
 module.exports.deleteEvent = (req, res, next) => {
   Event.remove({
@@ -57,6 +117,27 @@ module.exports.deleteEvent = (req, res, next) => {
 }
 
 
+/**
+ * GET /api/events/:_id
+ *
+ * @summary
+ * Get an event by its id
+ *
+ * @auth
+ * Requires authentication
+ *
+ * @param
+ * - id => String     - Event ID
+ *
+ * @returns:
+ * - Status 200 if successfully found
+ * - Status 401 if event not found
+ * * If successful, it returns a JSON of the following format:
+ * {
+ *  Event
+ * }
+*/
+
 module.exports.findEvent = (req, res, next) => {
   Event.findOne({
     _id: req.query._id,
@@ -70,6 +151,28 @@ module.exports.findEvent = (req, res, next) => {
     }
   });
 }
+
+/**
+ * POST /api/events/getEventsByLocation
+ *
+ * @summary
+ * Get an event by its city 
+ *
+ * @auth
+ * Requires authentication
+ *
+ * @param
+
+ * - query   => string  - a City
+ *
+ * @returns:
+ * - Status 200 if successfully found
+ * - Status 500 if error with finding events
+ *  * * If successful, it returns a JSON of the following format:
+ * {
+ *  /[ Event/]
+ * }
+*/
 
 
 module.exports.getEventsByLocation = (req, res, next) => {
@@ -88,6 +191,29 @@ module.exports.getEventsByLocation = (req, res, next) => {
     });
 }
 
+
+/**
+ * GET /api/events/getEventById
+ *
+ * @summary
+ * Get an event by its id
+ *
+ * @auth
+ * Requires authentication
+ *
+ * @param
+ * - id => String     - Event ID
+ *
+ * @returns:
+ * - Status 200 if successfully found
+ * - Status 400 if invalid id
+ * - Status 500 if error with deleting event
+ *  * * If successful, it returns a JSON of the following format:
+ * {
+ *  Event
+ * }
+
+*/
 module.exports.getEventById = (req, res) => {
   const id = req.params.id;
 
@@ -102,8 +228,32 @@ module.exports.getEventById = (req, res) => {
     .catch((err) => {
       return res.status(500).send(err);
     })
-}
 
+
+}
+/**
+ * POST '/api/events/getEventsByLocation
+ *
+ * @summary
+ * get events based on a query
+ *
+ * @auth
+ * Does require authentication 
+ *
+ * @param
+ * - event query => string   - name of event or key word
+ * - start_date      => string   - start date of event
+ * - end_date   => string   - end date of event
+ * - location   => city  - a string with the city 
+ *
+ * @returns:
+ * - Status 200 if event created successfully
+ * - Status 500 if bad request
+ * If successful, it returns a JSON of the following format:
+  * {
+  *   /[ Event/]
+  * }
+*/
 
 module.exports.getEventsByLocationAndDate = (req, res, next) => {
 
@@ -174,6 +324,28 @@ module.exports.getEventsByLocationAndDate = (req, res, next) => {
       res.status(500).send(err);
     });
 }
+
+
+/**
+ * PUT /api/events/update
+ *
+ * @summary
+ * Update the user's details
+ *
+ * @auth
+ * Requires authentication
+ *
+ * @param
+ * - name => string   - name of event
+ * - information  => string   - information
+ * - start_date      => string   - start date of event
+ * - end_date   => string   - end date of event
+ * - location   => {string, /[float /]}   - a string with the city and an array of coordinates
+ *
+ * @returns:
+ * - Status 200 if successful
+ * - Status 401 if event not found in database
+*/
 
 module.exports.updateEvent = (req, res, next) => {
   Event.findOne({
