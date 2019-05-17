0e813610be7cfa0b3053a63e249af934d12aecc9
Matty-Horton <mthorton1@sheffield.ac.uk>
2019-05-17 02:51:42 +0100

    Changed user card styling and links to profile

--- client/src/components/LoggedInNav/index.jsx
+++ client/src/components/LoggedInNav/index.jsx
@@ -17,8 +17,8 @@ class LoggedInNav extends Component {
   render() {
     return (
       <Nav className="logged-in-nav">
+        <Nav.Link href="/">Home</Nav.Link>
         <Nav.Link href="/discover">Discover</Nav.Link>
-        <Nav.Link href="/events">Events</Nav.Link>
         <Nav.Link href="#about">About</Nav.Link>
         <Nav.Link href="#contact">Contact</Nav.Link>
         <NavDropdown.Divider />
--- client/src/components/UserCard/index.jsx
+++ client/src/components/UserCard/index.jsx
@@ -3,15 +3,17 @@ import './index.css';
 import profilePlaceholder from './../../images/event-img-placeholder.jpg';
 
 const UserCard = (props) => {
-    const {username, bio} = props;
+    const {_id, username, bio} = props;
     return(
-        <div className="user-card-container">
-            <img className="user-card-profile" src={profilePlaceholder} alt="User Profile Picture"/>
-            <div className="user-card-info">
-                <h4 className="user-card-username">{username}</h4>
-                <p className="user-card-bio">{bio}</p>
+        <a href={`/users/${_id}`}>
+            <div className="user-card-container">
+                <img className="user-card-profile" src={profilePlaceholder} alt="User Profile Picture"/>
+                <div className="user-card-info">
+                    <h4 className="user-card-username">{username}</h4>
+                    <p className="user-card-bio">{bio}</p>
+                </div>
             </div>
-        </div>
+        </a>
     )
 }
 
